608d4e21a001c08b0d5f709851c8b34bd2a8a5d3
snqzspg <snqzspg@gmail.com>
2025-03-18 18:52:44 +0800

    Added usage statement

--- possible_license.hs
+++ possible_license.hs
@@ -28,13 +28,14 @@
 -- associated to meaningful or vulgar words.
 
 import Data.Attoparsec.ByteString.Char8 (isAlpha_ascii, isDigit)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
+import System.IO (hIsTerminalDevice, hPutStrLn, hSetBuffering, stderr, BufferMode (BlockBuffering), stdout)
 import Data.Bits ((.&.))
 import Data.Char (toUpper, ord)
 import Data.Array (Array)
 import Data.Array.IArray (array, (!))
 import Data.Generics (everything)
-import Data.List (intercalate, (\\))
+import Data.List (intercalate, (\\), insert)
 
 hasChecksum :: [Char] -> Bool
 hasChecksum plateStr = do
@@ -69,8 +70,8 @@ possibleThreeLetterPrefixes =
     [[a] ++ [b] ++ [c] |
         a <- ['S', 'F', 'G', 'P', 'T'],
         b <- ['B' .. 'Z'] \\ ['E', 'I', 'O', 'U'],
-        c <- ['A' .. 'Z']
-    ]
+        c <- ['A' .. 'Z'] \\ ['I', 'O']
+    ] ++ ["TIB"]
     -- (concatMap (\a -> 
     --         concatMap (\b -> map 
     --             (\c -> [a] ++ [b] ++ [c]) ['A' .. 'Z']
@@ -119,6 +120,7 @@ extractLetters = filter isAlpha_ascii
 possiblePlates :: [String] -> [String]
 possiblePlates matchPrefixes = do
     let plates = [i ++ j | i <- matchPrefixes, j <- map show [1..9999]]
+            \\ ["S" ++ show i | i <- [1..10]]
     map (\plateNoCS -> plateNoCS ++ [getChecksumLetter plateNoCS]) plates
 
 matchStr :: String -> String -> Bool
@@ -129,8 +131,8 @@ matchStr (c1:s1) (c2:s2)
     | c1 == c2 = matchStr s1 s2
     | otherwise = False
 
-extractPossibleLettersTrim :: String -> Int -> [String]
-extractPossibleLettersTrim trimmed trimmedLen
+extractPossibleLettersAfterTrim :: String -> Int -> [String]
+extractPossibleLettersAfterTrim trimmed trimmedLen
     | trimmedLen == 1 = [take 1 trimmed]
     | isDigit(trimmed !! 1) = [take 1 trimmed]
     | trimmedLen == 2 = [take 1 trimmed, trimmed]
@@ -139,8 +141,8 @@ extractPossibleLettersTrim trimmed trimmedLen
 
 extractPossibleLetters :: String -> [String]
 extractPossibleLetters incompleteStr = do
-    let possibleTrim = init $ init incompleteStr
-    extractPossibleLettersTrim possibleTrim $ length possibleTrim
+    let possibleTrim = init incompleteStr
+    extractPossibleLettersAfterTrim possibleTrim $ length possibleTrim
 
 possibleCombinationsThatMatches :: [Char] -> [[Char]]
 possibleCombinationsThatMatches pattern = do
@@ -150,16 +152,66 @@ possibleCombinationsThatMatches pattern = do
 
     filter (matchStr pattern) $ possiblePlates prefixPattern
 
-wrapLoadingText :: [Char] -> [Char]
-wrapLoadingText line = "\x1b[2K\x1b[G" ++ line ++ "\nThis is gonna take a while..."
+-- wrapLoadingText :: [Char] -> [Char]
+-- wrapLoadingText line =
+--     "\x1b[2K\x1b[G" ++ line ++ "\nThis is gonna take a while..."
+-- 
+-- wrapLoadingTextLines :: [[Char]] -> [[Char]]
+-- wrapLoadingTextLines = map wrapLoadingText
 
-wrapLoadingTextLines :: [[Char]] -> [[Char]]
-wrapLoadingTextLines = map wrapLoadingText
+insertHeaderAndIndent :: [Char] -> [[Char]] -> [[Char]]
+insertHeaderAndIndent pattern lines =
+    ["Possible matches for \"\x1b[1;32m" ++ pattern ++ "\x1b[0m\":"]
+        ++ map ("    " ++) lines
+        ++ [""]
+
+printUsage :: IO ()
+printUsage = do
+    arg0 <- getProgName
+    hPutStrLn stderr $ "usage: " ++ arg0 ++ " sg_plate_patterns ..."
+    hPutStrLn stderr $ "       sg_plate_patterns  A car plate number with "
+                    ++ "missing alphabets or numbers you want to search."
+    hPutStrLn stderr $ "                          To denote missing characters,"
+                    ++ " use \"?\"."
+    hPutStrLn stderr   "                          For example, \"SBA1234?\"."
+    hPutStrLn stderr $ "                          NOTE: Only accurate for "
+                    ++ "licence plates with checksums,"
+    hPutStrLn stderr $ "                                (Example: S1 - S10 does"
+                    ++ " not have checksums.)"
+    hPutStrLn stderr   ""
+    hPutStrLn stderr $ "This command will list out all possible matches for "
+                    ++ "each pattern specified."
+    hPutStrLn stderr  "DISCLAIMER: Not all valid license plates are added here."
+
+printUsageFormatted :: IO ()
+printUsageFormatted = do
+    arg0 <- getProgName
+    hPutStrLn stderr $ "\x1b[1;32musage\x1b[0m: " ++ arg0 ++ " sg_plate_patterns ..."
+    hPutStrLn stderr   "       sg_plate_patterns  A car plate number with missing alphabets or numbers you want to search."
+    hPutStrLn stderr   "                          To denote missing characters, use \"?\"."
+    hPutStrLn stderr   "                          For example, \"SBA1234?\"."
+    hPutStrLn stderr   "                          \x1b[1;34mNOTE\x1b[0m: Only accurate for licence plates with checksums,"
+    hPutStrLn stderr   "                                (Example: S1 - S10 does not have checksums.)"
+    hPutStrLn stderr   ""
+    hPutStrLn stderr   "This command will list out all possible matches for each pattern specified."
+    hPutStrLn stderr   "\x1b[1;33mDISCLAIMER\x1b[0m: Not all valid license plates are added here."
 
 main :: IO ()
 main = do
-    -- getArgs >>= print . map hasChecksum
-    -- getArgs >>= (\pureArgs -> putStrLn $ unlines (map (show . hasChecksum) pureArgs))
-    -- getArgs >>= putStrLn . unlines . map (show . extractChecksum . map toUpper)
+    -- getArgs >>= putStrLn . intercalate "\n"
+    --     . map (intercalate "\n" . possibleCombinationsThatMatches . map toUpper)
+    args <- getArgs
+    isErrTTY <- hIsTerminalDevice stderr
 
-    getArgs >>= putStrLn . intercalate "\n" . map (intercalate "\n" . possibleCombinationsThatMatches . map toUpper)
+    hSetBuffering stdout $ BlockBuffering Nothing
+    if null args
+    then
+        if isErrTTY then printUsageFormatted else printUsage
+    else
+        getArgs >>= putStrLn . intercalate "\n"
+            . map (\x -> 
+                intercalate "\n" 
+                    $ insertHeaderAndIndent x 
+                    $ possibleCombinationsThatMatches 
+                    $ map toUpper x
+            )
